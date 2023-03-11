aa3e9a6391c5f5b4d19b67b027f43e9e0c6b4f52
timo9036 <timo9036@hotmail.com>
2023-03-11 02:11:05 -0600

    practice

--- script.js
+++ script.js
@@ -56,3 +56,16 @@
 // console.log(scoreDolphins, scoreKoalas);
 // checkWinner(scoreDolphins, scoreDolphins);
 // -------------------------------------------------
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.lenth - 1]);
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.lenth - 1]),
+];
