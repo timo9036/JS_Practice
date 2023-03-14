f93313c349e3dcc7a45ef634616f487049516556
timo9036 <timo9036@hotmail.com>
2023-03-13 22:52:17 -0500

    asdf

--- script.js
+++ script.js
@@ -109,3 +109,13 @@
 // const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 // const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 // console.log(bills, tips, total);
+
+const jonas = {
+  //object literal
+  firstName: "jonas",
+  lastName: "schemetaman",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["mitchell", "peter", "steven"],
+};
+asdfasdf;
